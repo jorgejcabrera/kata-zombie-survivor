8255ef5f50a5ad666cb7847f21d31864dc1dfcc0
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2021-01-02 15:21:35 -0300

    basic test

---
+++ src/test/kotlin/NNNTest.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class NNNTest {
+
+    @Test
+    fun test() {
+        Assertions.assertEquals(
+            true, true
+        )
+    }
+}
